39ff7b77710f854f05293106ee37d7ef2596201f
Wenhao <wcao4@ualberta.ca>
2024-02-01 14:35:51 +0100

    server and client

--- client.py
+++ client.py
@@ -1,30 +1,67 @@
-# this is a distributed Client on localhost for the chatroom
-
-# import Modules
 import socket
 import threading
-import os
+import sys
+from time import sleep
+from distributed import host, port, send_multicast
 
-from distributed import host, port
+class ChatClient:
+    def __init__(self):
+        self.sock = None
+        self.connect_to_server()
 
-# create the TCP socket
-# create own ip address
-# bind the socket to the port
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-host_address = (host.get_ip(), port.multicast_server_port)
+    def connect_to_server(self):
+        """Connect to the chat server leader via multicast discovery."""
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+        server_exists = send_multicast.sending_join_chat_request_to_multicast()
+        if server_exists:
+            leader_address = (host.leader, port.server)
+            print(f'Connecting to server leader at {leader_address}')
+            self.sock.connect(leader_address)
+            self.sock.send('JOIN'.encode(host.unicode))
+            print("You've joined the chat room. Start chatting now!")
+        else:
+            print("No server available. Please try joining later.")
+            sys.exit(0)
 
+    def send_messages(self):
+        """Send messages to the server."""
+        while True:
+            message = input("")
+            try:
+                self.sock.send(message.encode(host.unicode))
+            except Exception as e:
+                print(e)
+                break
 
+    def receive_messages(self):
+        """Receive messages from the server."""
+        while True:
+            try:
+                data = self.sock.recv(host.buffer_size)
+                if not data:
+                    print("\nChat server unavailable. Reconnecting to new server leader in 3 seconds.")
+                    self.sock.close()
+                    sleep(3)
+                    self.connect_to_server()
+                else:
+                    print(data.decode(host.unicode))
+            except Exception as e:
+                print(e)
+                break
 
-# function for sending messages to the Server
-def send_message():
-    while True:
-        #get the message from the user
-        message = input("Enter a message: ")
-        #send the message to the Server
-        sock.sendall(bytes(message, host.get_unicode()))
-        #check if the user wants to quit
-        if message == 'q':
-            break
+    def run(self):
+        """Start threads for sending and receiving messages."""
+        threading.Thread(target=self.send_messages, daemon=True).start()
+        threading.Thread(target=self.receive_messages, daemon=True).start()
+        try:
+            while True: sleep(1)  # Keep the main thread alive.
+        except KeyboardInterrupt:
+            print("\nYou left the chat room.")
+            self.sock.close()
+            sys.exit(0)
 
+if __name__ == '__main__':
+    client = ChatClient()
+    client.run()
--- server.py
+++ server.py
@@ -1,53 +1,92 @@
-# this is a distributed Server on localhost for the chatroom
-
-# import Modules
 import socket
 import sys
 import threading
 import queue
+from distributed import host, port, receive_multicast, send_multicast, heartbeat
 
-from distributed import host, port
+class ChatServer:
+    def __init__(self):
+        self.host_address = (host.myIP, port.server)
+        self.sock = self.create_server_socket()
+        self.message_queue = queue.Queue()
+        self.client_list = []
+        self.initialize_server()
 
-#create the TCP socket
-#create own ip address
-#bind the socket to the port
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-host_address = (host.get_ip(), port.multicast_server_port)
+    def create_server_socket(self):
+        """Create and configure the server socket."""
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return sock
 
-#create a Queue for the messages
-FIFO_message_queue = queue.Queue()
+    def printer(self):
+        """Print server and client list information."""
+        print(f'\n[SERVER] Server List: {host.server_list} ==> Leader: {host.leader}'
+              f'\n[SERVER] Client List: {len(self.client_list)}'
+              f'\n[SERVER] Neighbour ==> {host.neighbour}\n', file=sys.stderr)
 
-#bind the socket to the port
-sock.bind(host_address)
-print("Server started on IP address: " + host.get_ip() + " and port: " + str(port.multicast_server_port))
+    def send_messages_to_clients(self):
+        """Send all messages from the queue to all connected clients."""
+        while not self.message_queue.empty():
+            message = self.message_queue.get()
+            for client in self.client_list:
+                client.send(message.encode(host.unicode))
 
-#listen for incoming connections
-sock.listen(5)
-
-#this is the thread for the server
-def server_thread():
-    while True:
-        #wait for a connection
-        print("Waiting for a connection...")
-        connection, client_address = sock.accept()
-        try:
-            print("Connection from: " + str(client_address))
-
-            #receive the data in small chunks and retransmit it
-            while True:
-                data = connection.recv(port.get_buffer_size())
-                if data:
-                    print("Received: " + str(data))
-                    #put the data into the FIFO queue
-                    FIFO_message_queue.put(data)
-                else:
-                    print("No more data from: " + str(client_address))
+    def client_handler(self, client, address):
+        """Handle incoming messages from connected clients."""
+        while True:
+            try:
+                data = client.recv(host.buffer_size)
+                if not data:
+                    self.handle_client_disconnection(client, address)
                     break
+                self.message_queue.put(f'{address} said: {data.decode(host.unicode)}')
+                print(f'Message from {address} ==> {data.decode(host.unicode)}')
+            except Exception as e:
+                print(e)
+                break
 
-        finally:
-            #clean up the connection
-            connection.close()
+    def handle_client_disconnection(self, client, address):
+        """Handle client disconnection."""
+        print(f'{address} disconnected')
+        self.client_list.remove(client)
+        client.close()
+        self.message_queue.put(f'\n{address} disconnected\n')
 
+    def start_server(self):
+        """Bind the TCP server socket and listen for connections."""
+        self.sock.bind(self.host_address)
+        self.sock.listen()
+        print(f'\n[SERVER] Starting and listening on {self.host_address}', file=sys.stderr)
+        while True:
+            client, address = self.sock.accept()
+            self.client_list.append(client)
+            threading.Thread(target=self.client_handler, args=(client, address), daemon=True).start()
 
-            
+    def initialize_server(self):
+        """Initialize the server and start the necessary threads."""
+        if not send_multicast.sending_request_to_multicast():
+            host.server_list.append(host.myIP)
+            host.leader = host.myIP
+
+        threading.Thread(target=receive_multicast.starting_multicast_receiver, daemon=True).start()
+        threading.Thread(target=heartbeat.start_heartbeat, daemon=True).start()
+        threading.Thread(target=self.start_server, daemon=True).start()
+
+        try:
+            while True:
+                if host.leader == host.myIP and (host.network_changed or host.replica_crashed):
+                    send_multicast.sending_request_to_multicast()
+                    host.leader_crashed = False
+                    host.network_changed = False
+                    host.replica_crashed = ''
+                    self.printer()
+                elif host.leader != host.myIP and host.network_changed:
+                    host.network_changed = False
+                    self.printer()
+                self.send_messages_to_clients()
+        except KeyboardInterrupt:
+            print("\nShutting down server.")
+            self.sock.close()
+
+if __name__ == '__main__':
+    server = ChatServer()
